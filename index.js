555122ff771f42db97aea4180e3883206270350f
Maksudul_Haque <smmaksudulhaque2000@gmail.com>
2025-02-21 04:03:15 +0600

    Added Task Delete

--- index.js
+++ index.js
@@ -97,6 +97,14 @@ async function run() {
       res.send(result);
     });
 
+    // Task Delete
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
 
 
 
